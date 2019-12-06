0768ee829badcdad4b972c26d1b333c89038c82c
lechemrc <roblecheminant@gmail.com>
2019-12-05 20:40:30 -0700

    add template

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +9,6 @@ def create_app():
 
     @app.route('/hello')
     def hello():
-        return 'Hello, World'
+        return render_template('base.html', title='hello')
 
     return app
