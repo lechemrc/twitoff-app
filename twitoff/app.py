399f7e31a856d674e26d820d6215261f75105f70
lechemrc <roblecheminant@gmail.com>
2019-12-05 20:23:42 -0700

    updating new twitoff folder with files

---
+++ twitoff/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return 'Index Page'
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World'
+    return app
